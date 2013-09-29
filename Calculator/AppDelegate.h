5f373e335f0ae84aa575a36b6af35a95a7de3e09
Hieu Tran <watdahieu@Hieus-MacBook-Pro.local>
2013-09-29 18:47:44 -0500

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by Hieu Tran on 9/29/13.
+//  Copyright (c) 2013 Hieu Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
